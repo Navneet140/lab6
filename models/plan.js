1ed653cb98bba137b10899806be235572d40e622
Navneet Singh <200413180@student.georgianc.on.ca>
2020-06-23 14:58:23 -0400

    done

--- models/plan.js
+++ models/plan.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const PlanSchema = new mongoose.Schema({
-  plan: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
